ccd5e3dbd9c2649be2e5c75ba76f1ad32942b172
Mathis <mathis.gaultier@etudiant.univ-rennes1.fr>
2024-10-14 16:32:31 +0200

    Better navbar

--- src/pages/Cartons.js
+++ src/pages/Cartons.js
@@ -1,13 +1,24 @@
 // src/pages/Cartons.js
-import React from 'react';
+import React, { useState } from 'react';
 import Carton from '../components/cartons';
 
 function Cartons() {
+  const [cartonKey, setCartonKey] = useState(0);
+
+  const nouveauCarton = () => {
+    setCartonKey(cartonKey + 1); // Update the key to re-render the Carton component
+  };
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',  }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>Cartons</h1>
       <p>Génère un carton exemple</p>
-      <Carton />
+      
+      {/* Render the Carton component */}
+      <Carton key={cartonKey} />
+      <button onClick={nouveauCarton} style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px', backgroundColor: 'orange', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+        Générer un nouveau carton
+      </button>
     </div>
   );
 }
